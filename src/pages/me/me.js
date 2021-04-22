21a2a27d239f1e052a44d91d16268a7e107329ca
Rodrigo Vellido <rodrigovellido1@gmail.com>
2021-04-22 14:41:37 -0300

    Update me.js

--- src/pages/me/me.js
+++ src/pages/me/me.js
@@ -21,7 +21,7 @@ const MePage = () => {
             >
                 <div className={meStyles.education}>
                     <p>
-                    Me gradue en el HENRY Bootcamp donde aprendí a través de +700hs de clases en vivo y prácticas constantes que me formaron como desarrollador web.
+                    Me gradué en el HENRY Bootcamp donde aprendí a través de +700 hs de clases en vivo y prácticas constantes que me formaron como desarrollador web.
                     Luego de la parte teórica-práctica del bootcamp realizamos dos proyectos de alcance real para afianzar lo aprendido, generar experiencia y acostumbrarnos a las dinámicas trabajo. 
                     </p>
                 </div>   
